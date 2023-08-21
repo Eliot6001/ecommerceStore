832f65e18415301d9b68bd3051af33f891553114
Arbaoui Djilali <jilali2004@gmail.com>
2023-08-21 11:02:53 +0100

    slight modifications

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,11 +9,12 @@ import ProductList from "@/components/ui/productList";
 export const revalidate = 0;
 const HelloStore = async () => {
   const billboard = await getBillboard("504650cf-9d71-40a1-8b75-97989aa6da33");
+  
   const Products = await getProducts({ isFeatured: true });
-  if(typeof billboard !== 'object') return null;
   return (
     <Container>
       <div className="space-y-10 pb-10">
+        {/**@ts-ignore */}
         <BillBoard data={billboard} />
 
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
